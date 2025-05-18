a0ae102ddc5da781c48691f02c55e40961bc3792
frisky-gh <frisky-.-gh@watao3.info>
2025-05-18 18:11:36 +0900

    add a language selection function.

--- show.php
+++ show.php
@@ -3,11 +3,19 @@
 // load libraries
 require("lib/common.php");
 
+$language_selector = null;
 try{
 	$setting = load_setting();
 	set_channel_of_log("show");
-	load_messagecatalog("app", $setting["web"]["lang"]);
 	log_info("load_setting: success.");
+
+	validate_inputs();
+	log_info("validate_inputs: success.", []);
+
+	$lang = $_COOKIE["lang"] ?? $setting["web"]["default_lang"];
+	$language_selector = generate_language_selector( $lang, $setting["web"]["lang_list"] );
+	load_messagecatalog("app", $lang);
+	log_info("load_messagecatalog: success.", ["lang" => $lang]);
 
 	$acceptedlist = load_acceptedlist();
 	log_info("load_acceptedlist: success.", []);
